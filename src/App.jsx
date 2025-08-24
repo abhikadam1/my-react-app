dbef62d4948a6b8c1216fb38291bf6a96f75f0a9
Abhi Kadam <kadamabhishek3188@gmail.com>
2025-08-25 00:17:00 +0530

    module css and props concept

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import Card from "./Card";
 import Footer from "./Footer";
+import Student from "./Student/Student";
 
 function App() {
     return (
         <>
+            <Student name="Abhi" age={30} isStudent={true} />
+            <Student name="" age="khuuh" isStudent={false} />
+            <Student  isStudent={true} />
+            <Student />
             <Card />
             <Card />
             <Card />
